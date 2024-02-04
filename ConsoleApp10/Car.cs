baf9861839c4040df7dc394d637e5304c1df2c97
Eugen Deur <eugendeur556@gmail.com>
2024-02-04 16:38:35 +0100

    Prvi dio projekta sejvan

---
+++ ConsoleApp10/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class Car
+    {   //private 
+        private string _name;//storing data
+        private int _hp;
+        private string _color;
+       public Car(string name, int hp = 0,string color = "black")
+        {
+            
+            _name = name;
+            Console.WriteLine(name+ "Car was created");
+            _hp = hp;
+            _color = color;
+        }
+        public void Drive()
+        {
+            Console.WriteLine(_name + " is driving");
+        }
+        public void Stopped()
+        {
+            Console.WriteLine(_name + " stopped");
+        }
+        public void Deatils()
+        {
+            Console.WriteLine("Car " +_name+ " has " +_hp+" and its "+_color);
+        }
+    }
+}
+
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("Audi A4", 280,"purple");
+            audi.Drive();
+            Car bmw = new Car("BMW M5",350);
+            bmw.Drive();
+            audi.Deatils();
+            bmw.Deatils();
+            
+            Console.WriteLine("Input 1 to stop bmw.");
+            Console.WriteLine("Input 2 to stop audi.");
+            
+            string userInput = Console.ReadLine();
+            
+            if (userInput== "1")
+            {
+                bmw.Stopped();
+            }
+            else if(userInput=="2")
+            {
+                audi.Stopped();
+            }
+            else
+            {
+                Console.WriteLine("Invalid number try again");
+            }
+            
+        }
+    }
+}
